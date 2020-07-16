0d6e11b20345cbb47a609a77fd4a759b29688cfa
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-07-16 16:38:12 -0300

    created base project

---
+++ Assets/Scripts/CarOrbit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarOrbit : MonoBehaviour
+{
+    float x;
+
+    [SerializeField]
+    float xSpeed = 120.0f;
+
+    [SerializeField]
+    float xMaxLimit = 360f;
+    void Start(){
+        Vector3 angles = transform.eulerAngles;
+        
+
+    } 
+
+    void LateUpdate(){
+        if(Input.GetMouseButton(0)){
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            Quaternion rotation = Quaternion.Euler(transform.eulerAngles.x, ClampAngle(x, 0, xMaxLimit), transform.eulerAngles.z);
+            transform.rotation = rotation;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
